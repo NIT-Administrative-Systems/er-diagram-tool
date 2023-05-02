9766cd1336c6a3093c24b84efc8d7907d621e052
Shift <shift@laravelshift.com>
2023-05-02 16:47:44 +0000

    Add return types to test methods

--- tests/LaravelERDTest.php
+++ tests/LaravelERDTest.php
@@ -7,7 +7,7 @@ use PHPUnit\Framework\Attributes\Test;
 class LaravelERDTest extends TestCase
 {
     #[Test]
-    public function true_is_true()
+    public function true_is_true(): void
     {
         $this->assertTrue(true);
     }
