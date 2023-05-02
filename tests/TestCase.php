af9be81520c310f66841e0a63b559c6feda7d9b7
Shift <shift@laravelshift.com>
2023-05-02 16:47:44 +0000

    Set return type of base TestCase methods
    From the [PHPUnit 8 release notes][1], the `TestCase` methods below now declare a `void` return type:
    
    - `setUpBeforeClass()`
    - `setUp()`
    - `assertPreConditions()`
    - `assertPostConditions()`
    - `tearDown()`
    - `tearDownAfterClass()`
    - `onNotSuccessfulTest()`
    
    [1]: https://phpunit.de/announcements/phpunit-8.html

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,7 +8,7 @@ use Kevincobain2000\LaravelERD\LaravelERDServiceProvider;
 
 class TestCase extends Orchestra
 {
-    public function setUp(): void
+    protected function setUp(): void
     {
         parent::setUp();
     }
